0fe620d7293f641e08c2e7fbdb33a5d78a6a487e
huy nguyen <huy.nguyen10@ntq-solution.com.vn>
2021-11-08 14:49:55 +0700

    update payment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-for (var i = 0; i < document.querySelectorAll(".btn").length; i++) {
+var numberOfButtons = document.querySelectorAll(".btn").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".btn")[i].addEventListener("click", function () {
         alert("Chức năng đặt hàng hiện chưa khả dụng. Chúng tôi sẽ cập nhật trong các bản update sau!");
     });
