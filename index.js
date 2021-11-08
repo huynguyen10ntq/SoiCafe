0b6768cac741b26cd2bdf0b53e26fc488cecf40a
huy nguyen <huy.nguyen10@ntq-solution.com.vn>
2021-11-08 13:39:53 +0700

    update payment info

---
+++ index.js
@@ -0,0 +1,5 @@
+for (var i = 0; i < document.querySelectorAll(".btn").length; i++) {
+    document.querySelectorAll(".btn")[i].addEventListener("click", function () {
+        alert("Chức năng đặt hàng hiện chưa khả dụng. Chúng tôi sẽ cập nhật trong các bản update sau!");
+    });
+};
