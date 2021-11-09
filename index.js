06c6148f65fa8a772af22423d8fa0b2474e46292
huy.nguyen10 <huy.nguyen10@ntq-solution.com.vn>
2021-11-09 23:26:47 +0700

    update NganDT's comment

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var numberOfButtons = document.querySelectorAll(".btn").length;
 
 for (var i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".btn")[i].addEventListener("click", function () {
-        alert("Chức năng đặt hàng hiện chưa khả dụng. Chúng tôi sẽ cập nhật trong các bản update sau!");
+        alert("Chức năng đặt hàng hiện chưa khả dụng. Chúng tôi sẽ cập nhật trong các bản update sau! Xin vui lòng liên hệ trực tiếp tới chủ shop để đặt hàng.");
     });
 };
